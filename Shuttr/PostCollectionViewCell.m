50d2073b2123e7b14c68c1b2c6628cf8a46d3692
PhiltheHen <philip.d.henson@gmail.com>
2015-10-23 07:37:45 -0700

    added custom collection view cell

---
+++ Shuttr/PostCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  PostCollectionViewCell.m
+//  Shuttr
+//
+//  Created by Philip Henson on 10/22/15.
+//  Copyright © 2015 MMInstaGroup. All rights reserved.
+//
+
+#import "PostCollectionViewCell.h"
+
+@implementation PostCollectionViewCell
+
+@end
--- Shuttr/PostPhotosViewController.m
+++ Shuttr/PostPhotosViewController.m
@@ -7,16 +7,36 @@
 //
 
 #import "PostPhotosViewController.h"
+#import "PostCollectionViewCell.h"
 
-@interface PostPhotosViewController ()
+@interface PostPhotosViewController () <UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
 @implementation PostPhotosViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];    
-    
+    [super viewDidLoad];
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+
+    PostCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
+
+    cell.imageView.image = [self.images objectAtIndex:indexPath.row];
+
+    return cell;
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.images.count;
+}
+
+- (IBAction)onPostButtonPressed:(UIButton *)sender {
+
+    //TODO: save parse data and perform unwind segue - maybe handle this all with delegation
 }
 
 @end
