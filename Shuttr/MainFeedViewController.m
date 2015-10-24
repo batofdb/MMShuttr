121474a7a4ec64b86909be083bdf8ffc2b2cae78
PhiltheHen <philip.d.henson@gmail.com>
2015-10-23 18:11:16 -0700

    added hard-coded images for testing

--- Shuttr/MainFeedViewController.m
+++ Shuttr/MainFeedViewController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
 
     // We have one test user for now
-    [PFUser logInWithUsername:@"francis" password:@"pizza"];
+    [PFUser logInWithUsernameInBackground:@"philly" password:@"cheesesteak"];
 
     // Call this anywhere in the code to get a reference to our test user
     PFUser *user = [PFUser currentUser];
--- Shuttr/PostPhotosViewController.m
+++ Shuttr/PostPhotosViewController.m
@@ -19,12 +19,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    // Hard coding images for now
+    self.images = @[[UIImage imageNamed:@"Arya_Stark"],
+                    [UIImage imageNamed:@"Cersei_Lannister"],
+                    [UIImage imageNamed:@"Daenerys_Targaryen"],
+                    [UIImage imageNamed:@"Eddard_Stark"],
+                    [UIImage imageNamed:@"GOT_default"],
+                    [UIImage imageNamed:@"Joffrey_Baratheon"],
+                    [UIImage imageNamed:@"Jon_Snow"],
+                    [UIImage imageNamed:@"Sansa_Stark"],
+                    [UIImage imageNamed:@"Tyrion_Lannister"]];
 }
 
 -(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-
     PostCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
-
     cell.imageView.image = [self.images objectAtIndex:indexPath.row];
 
     return cell;
