70ba3c495a4cb787f8fc8e1f8c86b398b20b4584
PhiltheHen <philip.d.henson@gmail.com>
2015-10-23 21:09:43 -0700

    added SearchViewController and ProfileViewController

---
+++ Shuttr/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Shuttr
+//
+//  Created by Philip Henson on 10/23/15.
+//  Copyright © 2015 MMInstaGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
---
+++ Shuttr/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  Shuttr
+//
+//  Created by Philip Henson on 10/23/15.
+//  Copyright © 2015 MMInstaGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
